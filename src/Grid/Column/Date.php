b7342048f11861cb163a3ad926c0068d46c3bf58
s16121986 <s16121986@yandex.ru>
2022-01-07 15:36:38 +0300

    Update Date.php

--- src/Grid/Column/Date.php
+++ src/Grid/Column/Date.php
@@ -11,7 +11,15 @@ class Date extends AbstractColumn {
 	];
 
 	public function formatValue($value, $row = null) {
-		return parent::formatValue((new DateTime($value))->format($this->format), $row);
+		if (is_string($value)) {
+			if (preg_match('/^\d{4}-\d{2}-\d{2}$/', $value))
+				$value = new \DateTime($value);
+			else
+				$value = new DateTime($value);
+		} else
+			$value = new DateTime($value);
+
+		return parent::formatValue($value->format($this->format), $row);
 	}
 
 }
