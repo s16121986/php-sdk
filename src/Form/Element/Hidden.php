a312572767ca6a40457a0800ef63232951a7794f
s16121986 <s16121986@yandex.ru>
2022-09-10 13:24:19 +0300

    Update Hidden.php

--- src/Form/Element/Hidden.php
+++ src/Form/Element/Hidden.php
@@ -5,7 +5,14 @@ namespace Gsdk\Form\Element;
 class Hidden extends AbstractInput {
 
 	protected $options = [
-		'inputType' => 'hidden'
+		'inputType' => 'hidden',
+		'nullValue' => ''
 	];
+
+	protected function prepareValue($value) {
+		if ($this->nullValue === $value)
+			$value = null;
+		return parent::prepareValue($value);
+	}
 
 }
