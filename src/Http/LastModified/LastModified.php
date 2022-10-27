956e4a91caf2684d1be628a92ffd191aa81606a5
s16121986 <s16121986@yandex.ru>
2022-10-27 13:44:54 +0300

    Update LastModified.php

--- src/Http/LastModified/LastModified.php
+++ src/Http/LastModified/LastModified.php
@@ -6,7 +6,15 @@ class LastModified {
 
 	private ?\DateTime $updatedAt = null;
 
-	public function set(?\DateTime $updatedAt): void {
+	public function set($updatedAt): void {
+		if (is_string($updatedAt))
+			$updatedAt = new \DateTime($updatedAt);
+		else if (is_int($updatedAt)) {
+			$updatedAt = new \DateTime();
+			$updatedAt->setTimestamp($updatedAt);
+		} else if (!$updatedAt instanceof \DateTime)
+			throw new \Exception('DateTime format required');
+
 		$this->updatedAt = $updatedAt;
 	}
 
