fe90fcb3d2377d74b8060e56f898a907d1f6962b
s16121986 <s16121986@yandex.ru>
2022-11-05 15:58:34 +0300

    Update Email.php

--- src/Form/Element/Email.php
+++ src/Form/Element/Email.php
@@ -7,7 +7,7 @@ class Email extends AbstractInput {
 	protected $options = [
 		'inputType' => 'email'
 	];
-	protected $attributes = ['maxlength', 'minlength', 'multiple', 'pattern', 'placeholder', 'readonly', 'size'];
+	protected $attributes = ['maxlength', 'autocomplete', 'minlength', 'multiple', 'pattern', 'placeholder', 'readonly', 'size'];
 
 	public function checkValue($value) {
 		return (bool)filter_var($value, FILTER_VALIDATE_EMAIL);
